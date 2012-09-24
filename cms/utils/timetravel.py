fad6a570a49c84181b19a36dda16e458786e8895
Janneke Janssen <j.janssen@lukkien.com>
2012-09-24 16:45:26 +0200

    Added refdate to timetravel util
    Added refdate GET param check into get_reference_date util. So external
    requests can be made to our CMS Pages.

--- cms/utils/timetravel.py
+++ cms/utils/timetravel.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from datetime import datetime
 
 
@@ -5,14 +6,15 @@ def get_reference_date(request):
     """
     Returns the reference date to be used for determining whether content
     (Pages and Plugins) are "published".
-    
+
     If the user is "time travelling" (and therefore a "timetravel_date" session
     variable has been set), the time travel date is used. Otherwise, it uses
     the current date/time (the standard Django CMS behavior).
     """
-    if 'timetravel_date' in request.session:
+    if 'refdate' in request.GET and settings.ENVIRONMENT != 'prd':
+        # For timetravel at AH.nl. Same security as AH.nl: allow everywhere except at prd.
+        return datetime.strptime(request.GET['refdate'], '%Y-%m-%dT%H:%M:%S')  # e.g. refdate=2012-05-04T09:00:00
+    elif 'timetravel_date' in request.session:
         return request.session['timetravel_date']
     else:
         return datetime.now()
-    
-    
